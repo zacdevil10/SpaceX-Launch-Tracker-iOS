aba82768b2930de310130a95aaeb6ca9410f18fe
Zac Hadjineophytou <zac@zac-h.co.uk>
2023-08-10 20:07:19 +0100

    Initial Commit

---
+++ SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Zac Hadjineophytou on 10/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
