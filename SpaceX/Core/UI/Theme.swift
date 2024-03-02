8d3abe3687fb1a706a4c6c2069277be8e08ee1f5
Zac Hadjineophytou <zac@zac-h.co.uk>
2024-03-02 15:49:29 +0000

    Added material 3 theme

---
+++ SpaceX/Core/UI/Theme.swift
@@ -0,0 +1,74 @@
+//
+//  Theme.swift
+//  SpaceX
+//
+//  Created by Zac Hadjineophytou on 02/03/2024.
+//
+
+import SwiftUI
+
+struct Colors {
+    var primary: Color
+    var onPrimary: Color
+    var primaryContainer: Color
+    var onPrimaryContainer: Color
+    var secondary: Color
+    var onSecondary: Color
+    var secondaryContainer: Color
+    var onSecondaryContainer : Color
+    var tertiary : Color
+    var onTertiary : Color
+    var tertiaryContainer : Color
+    var onTertiaryContainer : Color
+    var error : Color
+    var onError : Color
+    var errorContainer : Color
+    var onErrorContainer : Color
+    var outline : Color
+    var background : Color
+    var onBackground : Color
+    var surface : Color
+    var onSurface : Color
+    var surfaceVariant : Color
+    var onSurfaceVariant : Color
+    var inverseSurface : Color
+    var inverseOnSurface : Color
+    var inversePrimary : Color
+    var shadow : Color
+    var surfaceTint : Color
+    var outlineVariant : Color
+    var scrim : Color
+}
+
+extension Color {
+    static var primary: Color = Color("Primary")
+    static var onPrimary: Color = Color("OnPrimary")
+    static var primaryContainer: Color = Color("PrimaryContainer")
+    static var onPrimaryContainer: Color = Color("OnPrimaryContainer")
+    static var secondary: Color = Color("Secondary")
+    static var onSecondary: Color = Color("OnSecondary")
+    static var secondaryContainer: Color = Color("SecondaryContainer")
+    static var onSecondaryContainer: Color = Color("OnSecondaryContainer")
+    static var tertiary : Color = Color("Tertiary")
+    static var onTertiary : Color = Color("OnTertiary")
+    static var tertiaryContainer : Color = Color("TertiaryContainer")
+    static var onTertiaryContainer : Color = Color("OnTertiaryContainer")
+    static var error : Color = Color("Error")
+    static var onError : Color = Color("OnError")
+    static var errorContainer : Color = Color("ErrorContainer")
+    static var onErrorContainer : Color = Color("OnErrorContainer")
+    static var outline : Color = Color("Outline")
+    static var background : Color = Color("Background")
+    static var onBackground : Color = Color("OnBackground")
+    static var surface : Color = Color("Surface")
+    static var onSurface : Color = Color("OnSurface")
+    static var surfaceVariant : Color = Color("SurfaceVariant")
+    static var onSurfaceVariant : Color = Color("OnSurfaceVariant")
+    static var inverseSurface : Color = Color("InverseSurface")
+    static var inverseOnSurface : Color = Color("InverseOnSurface")
+    static var inversePrimary : Color = Color("InversePrimary")
+    static var shadow : Color = Color("Shadow")
+    static var surfaceTint : Color = Color("SurfaceTint")
+    static var outlineVariant : Color = Color("OutlineVariant")
+    static var scrim : Color = Color("Scrim")
+}
